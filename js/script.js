0a3ce415ad3f189eb1fdb15f6a79b16618ddd9ce
brianmuigai96 <brian.muigai@student.moringaschool.com>
2021-06-30 14:35:37 +0300

    adding toggle effect to the what we do section

--- js/script.js
+++ js/script.js
@@ -1,28 +1,23 @@
-          $(document).ready(function()
-         {$(".time").hover(function()
-         {$(this).animate({"opacity":0.3},500)
-         },
-         function()
-         {$(this).animate({"opacity":1},500)});
-         });
+$(document).ready(function(){
+  $("#design-img").click(function(){
+      $("#design-text").show() && $("#design-img").hide() && $("#head1").hide();
+  });
+  $("#design-text").click(function(){
+      $("#design-text").hide() && $("#design-img").show() && $("#head1").show();
+  });
+  $("#development-img").click(function(){
+      $("#development-text").show() && $("#development-img").hide() && $("#head2").hide();
+  });
+  $("#development-text").click(function(){
+      $("#development-text").hide() && $("#development-img").show() && $("#head2").show();
+  });
+  $("#product-img").click(function(){
+      $("#product-text").show()&&  $("#product-img").hide() && $("#head3").hide();{
+      }
+  });
+  $("#product-text").click(function(){
+      $("#product-text").hide() && $("#product-img").show() && $("#head3").show();
+  });
 
 
-         $("#design-img").click(function(){
-             $(".design-text").show() && $("#design-img").hide() && $("#head1").hide();
-         });
-         $("#design2").click(function(){
-             $("#design2").hide() && $("#fig1").show() && $("#caption1").show();
-         });
-         $("#fig2").click(function(){
-             $("#development2").show() && $("#fig2").hide() && $("#caption2").hide();
-         });
-         $("#development2").click(function(){
-             $("#development2").hide() && $("#fig2").show() && $("#caption2").show();
-         });
-         $("#fig3").click(function(){
-             $("#productmanagement2").show()&&  $("#fig3").hide() && $("#caption3").hide();{
-             }
-         });
-         $("#productmanagement2").click(function(){
-             $("#productmanagement2").hide() && $("#fig3").show() && $("#caption3").show();
-         });
+});
